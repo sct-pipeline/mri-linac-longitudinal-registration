ef1c34388b83629eafec7c05132d721b5fefd2d2
Nilser3 <nilser.laines@gmail.com>
2024-06-28 19:49:51 -0400

    update registration script, using labels and segmentations

--- scripts/registration_and_evaluation.sh
+++ scripts/registration_and_evaluation.sh
@@ -21,49 +21,91 @@ register_and_evaluate() {
     local acq_suffix=$2
     local sub=$(printf "%03g" $subject)
 
-    # Rigid registration
+    # 1. Vertebral labeling ses-1 and ses-2
+    sct_label_vertebrae -i "$bids_file/sub-$sub/ses-1/anat/sub-${sub}_ses-1_acq-${acq_suffix}_T2w.nii.gz" \
+                        -s "$bids_file/derivatives/labels/sub-$sub/ses-1/anat/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" -c t2 \
+                        -ofolder label_vertebrae                     
+    sct_label_vertebrae -i "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
+                        -s "$bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" -c t2 \
+                        -ofolder label_vertebrae
+    # 2. SC mask dilation
+    sct_maths           -i "$bids_file/derivatives/labels/sub-$sub/ses-1/anat/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" -dilate 2 \
+                        -o "label_vertebrae/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz"
+    sct_maths           -i "$bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" -dilate 2 \
+                        -o "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz"
+    # 3.Rigid registration
     sct_register_multimodal -i "$bids_file/sub-$sub/ses-1/anat/sub-${sub}_ses-1_acq-${acq_suffix}_T2w.nii.gz" \
                             -d "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
-                            -ofolder sct_rigid -param step=1,type=im,algo=rigid,metric=MI,iter=100,shrink=2
-
-    # Rigid + affine + syn registration
+                            -ilabel "label_vertebrae/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_vlabels.nii.gz" \
+                            -dlabel "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_vlabels.nii.gz" \
+                            -iseg   "label_vertebrae/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" \
+                            -dseg   "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" \
+                            -param step=0,type=label,algo=syn,metric=CC,iter=0:step=1,type=seg,algo=slicereg,metric=MeanSquares:step=2,type=im,algo=rigid,metric=CC \
+                            -ofolder sct_rigid
+    # Syn registration
     sct_register_multimodal -i "$bids_file/sub-$sub/ses-1/anat/sub-${sub}_ses-1_acq-${acq_suffix}_T2w.nii.gz" \
                             -d "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
-                            -ofolder sct_syn -param step=1,type=im,algo=rigid,metric=MI,iter=100,shrink=2:step=2,type=im,algo=affine,metric=MI,iter=100,shrink=2:step=3,type=im,algo=syn,metric=MI,iter=100,shrink=2
-
-    # Rigid + affine + syn + dl registration
+                            -ilabel "label_vertebrae/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_vlabels.nii.gz" \
+                            -dlabel "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_vlabels.nii.gz" \
+                            -iseg   "label_vertebrae/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" \
+                            -dseg   "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" \
+                            -param step=0,type=label,algo=syn,metric=CC,iter=0:step=1,type=seg,algo=slicereg,metric=MeanSquares:step=2,type=im,algo=syn,metric=CC \
+                            -ofolder sct_syn 
+    # Dl registration
     sct_register_multimodal -i "$bids_file/sub-$sub/ses-1/anat/sub-${sub}_ses-1_acq-${acq_suffix}_T2w.nii.gz" \
                             -d "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
-                            -ofolder sct_dl -param step=1,type=im,algo=rigid,metric=MI,iter=100,shrink=2:step=2,type=im,algo=affine,metric=MI,iter=100,shrink=2:step=3,type=im,algo=syn,metric=MI,iter=100,shrink=2:step=4,type=im,algo=dl
+                            -ilabel "label_vertebrae/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_vlabels.nii.gz" \
+                            -dlabel "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_vlabels.nii.gz" \
+                            -iseg   "label_vertebrae/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" \
+                            -dseg   "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" \
+                            -param step=0,type=label,algo=syn,metric=CC,iter=0:step=1,type=seg,algo=slicereg,metric=MeanSquares:step=2,type=im,algo=dl,metric=CC \
+                            -ofolder sct_dl
+    # QC generation
+    sct_qc                  -i $bids_file/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w.nii.gz"  \
+                            -s $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz" \
+                            -d  $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz" \
+                            -p sct_deepseg_lesion -plane sagittal -qc qc-non-FatSup
+    sct_qc                  -i $bids_file/sub-$sub/ses-2/anat/sub-$sub"_ses-1_acq-ax_desc-registered_T2w.nii.gz"  \
+                            -s $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -d $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -p sct_deepseg_lesion -plane sagittal -qc qc-non-FatSup
+    sct_qc                  -i sct_rigid/sub-$sub"_ses-1_acq-ax_T2w_reg.nii.gz"  \
+                            -s $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -d $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -p sct_deepseg_lesion -plane sagittal -qc qc-non-FatSup
+    sct_qc                  -i sct_syn/sub-$sub"_ses-1_acq-ax_T2w_reg.nii.gz"  \
+                            -s $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -d $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -p sct_deepseg_lesion -plane sagittal -qc qc-non-FatSup
+    sct_qc                  -i sct_dl/sub-$sub"_ses-1_acq-ax_T2w_reg.nii.gz"  \
+                            -s $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -d $bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-$sub"_ses-2_acq-ax_T2w_label-SC_seg.nii.gz"  \
+                            -p sct_deepseg_lesion -plane sagittal -qc qc-non-FatSup
 
-    # Calculate similarity on full FOV images
-    python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
-                                           -ses1reg_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-1_acq-${acq_suffix}_desc-registered_T2w.nii.gz" \
-                                           -acquisition T2w -method challenge -o "metrics/challenge_sub-${sub}_ses-2_acq-${acq_suffix}_T2w.csv"
-    python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
-                                           -ses1reg_file "sct_rigid/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_reg.nii.gz" \
-                                           -acquisition T2w -method sct_rigid -o "metrics/rigid_sub-${sub}_ses-2_acq-${acq_suffix}_T2w.csv"
-    python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
-                                           -ses1reg_file "sct_syn/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_reg.nii.gz" \
-                                           -acquisition T2w -method sct_syn -o "metrics/syn_sub-${sub}_ses-2_acq-${acq_suffix}_T2w.csv"
-    python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
-                                           -ses1reg_file "sct_dl/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_reg.nii.gz" \
-                                           -acquisition T2w -method sct_dl -o "metrics/dl_sub-${sub}_ses-2_acq-${acq_suffix}_T2w.csv"
-
+    # 5.Registration to PAM50 
+    sct_register_to_template    -i      "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
+                                -ldisc  "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_vlabels.nii.gz" \
+                                -s      "label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz" \
+                                -c      t2 -ofolder sct_pam50
+    
     # Calculate similarity masked by the SC
-    local mask_file="$bids_file/derivatives/labels/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz"
+    local mask_file="label_vertebrae/sub-${sub}_ses-2_acq-${acq_suffix}_T2w_label-SC_seg.nii.gz"
     python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
                                            -ses1reg_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-1_acq-${acq_suffix}_desc-registered_T2w.nii.gz" \
-                                           -acquisition T2w -method challenge -o "metrics/challenge_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv" -mask_file "$mask_file"
+                                           -acquisition T2w -method challenge  -mask_file "$mask_file" \
+                                           -o "metrics/challenge_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv"
     python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
                                            -ses1reg_file "sct_rigid/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_reg.nii.gz" \
-                                           -acquisition T2w -method sct_rigid -o "metrics/rigid_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv" -mask_file "$mask_file"
+                                           -acquisition T2w -method sct_rigid  -mask_file "$mask_file" \
+                                           -o "metrics/rigid_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv"
     python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
                                            -ses1reg_file "sct_syn/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_reg.nii.gz" \
-                                           -acquisition T2w -method sct_syn -o "metrics/syn_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv" -mask_file "$mask_file"
+                                           -acquisition T2w -method sct_syn  -mask_file "$mask_file" \
+                                           -o "metrics/syn_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv"
     python calculate_similarity_metrics.py -ses2_file "$bids_file/sub-$sub/ses-2/anat/sub-${sub}_ses-2_acq-${acq_suffix}_T2w.nii.gz" \
                                            -ses1reg_file "sct_dl/sub-${sub}_ses-1_acq-${acq_suffix}_T2w_reg.nii.gz" \
-                                           -acquisition T2w -method sct_dl -o "metrics/dl_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv" -mask_file "$mask_file"
+                                           -acquisition T2w -method sct_dl -mask_file "$mask_file" \
+                                           -o "metrics/dl_sub-${sub}_ses-2_acq-${acq_suffix}_T2w_masked.csv" 
 }
 
 # Subjects IDs of T2w non-fat-suppressed images
@@ -75,9 +117,9 @@ for subject in "${non_fat_sup_subjects[@]}"; do
 done
 
 # Subjects IDs of T2w fat-suppressed images
-fat_sup_subjects=(11 13 17 21 22 25 29 31 32 34 36 39 41 42 47 50 52 53 54 57 59 62 65 67 68 70 73 77 78 79 81 82 85 88 89 92 98 100 102 103 105 106 110 115 116 117 119 121 123 124 126 127 132 133 134 135 137 140 143 147 148 151 155 156 157 160 162 165 166 167 168 170 172 173 178 179 182 184 186 191 192 193 194 195)
+#fat_sup_subjects=(11 13 17 21 22 25 29 31 32 34 36 39 41 42 47 50 52 53 54 57 59 62 65 67 68 70 73 77 78 79 81 82 85 88 89 92 98 100 102 103 105 106 110 115 116 117 119 121 123 124 126 127 132 133 134 135 137 140 143 147 148 151 155 156 157 160 162 165 166 167 168 170 172 173 178 179 182 184 186 191 192 193 194 195)
 
 # Registration and evaluation for fat-suppressed images
-for subject in "${fat_sup_subjects[@]}"; do
-    register_and_evaluate $subject "ax-FS"
-done
+#for subject in "${fat_sup_subjects[@]}"; do
+#    register_and_evaluate $subject "ax-FS"
+#done
